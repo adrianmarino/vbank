9e2006b39451150a6c8e83644954a07d96ec6d67
Adrian Norberto Marino <adrianmarino@gmail.com>
2016-02-27 11:33:57 -0300

    delete account method

--- src/main/java/com/nonosoft/vbank/domain/services/AccountCrudService.java
+++ src/main/java/com/nonosoft/vbank/domain/services/AccountCrudService.java
@@ -10,12 +10,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class AccountCrudService {
 
+    public Page<Account> findByPage(Integer page) {
+        return repository.findAll(new PageRequest(page, SIZE));
+    }
+
     public Account create(String code) {
         return repository.save(new Account(code));
     }
 
-    public Page<Account> findByPage(Integer page) {
-        return repository.findAll(new PageRequest(page, SIZE));
+    public void remove(String code) {
+        repository.delete(repository.findByCode(code));
     }
 
     private static final int SIZE = 20;
--- src/main/java/com/nonosoft/vbank/http/resources/AccountResource.java
+++ src/main/java/com/nonosoft/vbank/http/resources/AccountResource.java
@@ -1,4 +1,4 @@
-package com.nonosoft.vbank.http.controller;
+package com.nonosoft.vbank.http.resources;
 
 import com.nonosoft.vbank.domain.models.Account;
 import com.nonosoft.vbank.http.response.ResponseUtils;
@@ -15,12 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 @RequestMapping("/accounts")
-public class AccountController {
+public class AccountResource {
+
+    @RequestMapping(value = "/pages/{page}")
+    public ResponseEntity<List<Account>> pages(@PathVariable Integer page) {
+        return ResponseUtils.toPageResponse(service.findByPage(page));
+    }
 
     @RequestMapping(method = POST)
     public ResponseEntity<Void> create(@RequestBody CreateAccountRequest request) {
@@ -28,9 +34,10 @@ public class AccountController {
         return new ResponseEntity<>(OK);
     }
 
-    @RequestMapping(value = "/pages/{page}", method = GET)
-    public ResponseEntity<List<Account>> pages(@PathVariable Integer page) {
-        return ResponseUtils.toPageResponse(service.findByPage(page));
+    @RequestMapping(value = "/{code}", method = DELETE)
+    public ResponseEntity<Void> remove(@PathVariable String code) {
+        service.remove(code);
+        return new ResponseEntity<>(OK);
     }
 
     @Autowired
