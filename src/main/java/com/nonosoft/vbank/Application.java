2ec4b780e183124b276e9f4c41544b581a5a7db7
Adrian Norberto Marino <adrianmarino@gmail.com>
2016-02-29 20:13:03 -0300

    generate war for production app server

--- src/main/java/com/nonosoft/vbank/Application.java
+++ src/main/java/com/nonosoft/vbank/Application.java
@@ -4,14 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 import static java.lang.System.getProperty;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    logger.info("<< Environment: {} >>", getProperty("spring.profiles.active"));
+    return application.sources(Application.class);
+  }
 
   public static void main(String[] args) {
-    logger.info("<< Profile: {} >>", getProperty("spring.profiles.active"));
+    logger.info("<< Environment: {} >>", getProperty("spring.profiles.active"));
     SpringApplication.run(Application.class, args);
   }
 
--- src/test/groovy/com/nonosoft/vbank/test/resources/impl/AccountResourceSpec.groovy
+++ src/test/groovy/com/nonosoft/vbank/test/resources/impl/AccountResourceSpec.groovy
@@ -5,50 +5,50 @@ import com.nonosoft.vbank.test.resources.ResourceSpec
 class AccountResourceSpec extends ResourceSpec {
   def "create a new account"() {
     given: "an account code"
-    def code = '123'
+      def code = '123'
     when: "create a new account with the code"
-    client.createAccount(code)
+      client.createAccount(code)
     then: "should exits a new account with the code"
-    client.accountByCode(code).code == code
+      client.accountByCode(code).code == code
     cleanup:
-    client.deleteAccount(code)
+      client.deleteAccount(code)
   }
 
   def "update account code"() {
     given: "a pre-existent account"
-    def code = '123'
-    client.createAccount(code)
+      def code = '123'
+      client.createAccount(code)
     and: "a new code"
-    def newCode = 'juan-perez'
+      def newCode = 'juan-perez'
     when: "update account with the new code"
-    client.updateAccount(code, newCode)
+      client.updateAccount(code, newCode)
     then: "shouldn't exits an account with old code"
-    client.accountByCode(code) == null
+      client.accountByCode(code) == null
     and: "should exist an account with the new code"
-    client.accountByCode(newCode) != null
+      client.accountByCode(newCode) != null
     cleanup:
-    client.deleteAccount(newCode)
+      client.deleteAccount(newCode)
   }
 
   def "delete an account"() {
     given: "a pre-existent account"
-    def code = '123'
-    client.createAccount(code)
+      def code = '123'
+      client.createAccount(code)
     when: "delete the account with a code"
-    client.deleteAccount(code)
+      client.deleteAccount(code)
     then: "shouldn't exits any account with the code"
-    client.accountByCode(code) == null
+      client.accountByCode(code) == null
   }
 
   def "get accounts page"() {
     given: "a pre-existent account"
-    def code = '123'
-    client.createAccount(code)
+      def code = '123'
+      client.createAccount(code)
     when: "get first accounts page"
-    def page = client.accountsOnFirstPage()
+      def page = client.accountsOnFirstPage()
     then: "the page should contains the account"
-    page.any { it.code == code }
+      page.any { it.code == code }
     cleanup:
-    client.deleteAccount(code)
+      client.deleteAccount(code)
   }
 }
