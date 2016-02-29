30bb67c145a4703b05392cdbca4ea99ab79a8d72
Adrian Norberto Marino <adrianmarino@gmail.com>
2016-02-29 19:48:18 -0300

    remove unused exception

--- src/main/java/com/nonosoft/vbank/env/MissingVBankEnvironmentException.java
+++
@@ -1,8 +0,0 @@
-package com.nonosoft.vbank.env;
-
-public class MissingVBankEnvironmentException extends RuntimeException {
-  public MissingVBankEnvironmentException() {
-    super("Missing environment value. Try use any of next (case insensitive) values:" +
-        " env=[ \"dev\" | \"test\" | \"prod\" ]");
-  }
-}
